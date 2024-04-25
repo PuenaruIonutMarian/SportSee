80f8488cd219620aef984bdd45d0b177d4688b20
Pue90 <43031720+PuenaruIonutMarian@users.noreply.github.com>
2024-04-25 09:25:02 +0200

    Base of graphs

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,43 +1,36 @@
+import React from 'react';
 import CaloriesIcon from '../../assets/calories-icon.svg';
 import ProteinIcon from '../../assets/protein-icon.svg';
 import CarbsIcon from '../../assets/carbs-icon.svg';
 import FatIcon from '../../assets/fat-icon.svg';
-import style from './Results.module.scss'
+import style from './Results.module.scss';
 
 const Results = ({ calorieCount, proteinCount, carbohydrateCount, lipidCount }) => {
-  const hasData = calorieCount || proteinCount || carbohydrateCount || lipidCount;
-
   return (
     <div className={style.results}>
-      {hasData && (
-        <>
-          <div className={style.resultsDataItem}>
-            <img src={CaloriesIcon} alt="Calories Icon" />
-            <span>{calorieCount} kcal</span>
-            <p>Calories</p>
-          </div>
-          <div className={style.resultsDataItem}>
-            <img src={ProteinIcon} alt="Protein Icon" />
-            <span>{proteinCount} g</span>
-            <p>Proteines</p>
-          </div>
-          <div className={style.resultsDataItem}>
-            <img src={CarbsIcon} alt="Carbohydrates Icon" />
-            <span>{carbohydrateCount} g</span>
-            <p>Glucides</p>
-          </div>
-          <div className={style.resultsDataItem}>
-            <img src={FatIcon} alt="Fat Icon" />
-            <span>{lipidCount} g</span>
-            <p>Lipides</p>
-          </div>
-        </>
-      )}
-      {!hasData && (
-        <p className={style.noData}>Aucun information</p>
-      )}
+      {renderDataItem(calorieCount, CaloriesIcon, 'Calories', 'kcal')}
+      {renderDataItem(proteinCount, ProteinIcon, 'Proteines', 'g')}
+      {renderDataItem(carbohydrateCount, CarbsIcon, 'Glucides', 'g')}
+      {renderDataItem(lipidCount, FatIcon, 'Lipides', 'g')}
+    </div>
+  );
+};
+
+const renderDataItem = (dataCount, icon, label, unit) => {
+  return dataCount ? (
+    <div className={style.resultsDataItem}>
+      <img src={icon} alt={`${label} Icon`} />
+      <div>
+        <span>{dataCount} {unit}</span>
+        <p>{label}</p>
+      </div>
+    </div>
+  ) : (
+    <div className={style.resultsDataItem}>
+      <p className={style.noData}>Aucun information</p>
     </div>
   );
 };
 
 export default Results;
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Results from '../../components/Results/Results'
 
 const Home = () => {
   return (
-    <>
+    <main className="home">
     <Header name="John"/>
-    </>
+    <Results calorieCount={10} proteinCount={0.0} carbohydrateCount={0.5} />
+
+    </main>
   )
 }
 
